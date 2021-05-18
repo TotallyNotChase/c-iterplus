51d7107a1b348b4566ea299ba0c640c0871a6557
Chase <44284917+TotallyNotChase@users.noreply.github.com>
2021-05-18 22:10:08 +0700

    Add documentation for the `take` abstraction

--- include/take.h
+++ include/take.h
@@ -1,3 +1,13 @@
+/**
+ * @file
+ * @brief Macros for implementing the `take` abstraction using the `IterTake` struct.
+ *
+ * https://hackage.haskell.org/package/base-4.15.0.0/docs/Data-List.html#v:take
+ * An IterTake struct is a struct that keeps track of how many elements have been extracted out of an iterator
+ * as well as a limit to stop at - the iterator impl for this struct will keep consuming from an iterator until
+ * it hits that limit, or until the source iterable gets exhausted, whichever comes first.
+ */
+
 #ifndef IT_TAKE_H
 #define IT_TAKE_H
 
@@ -5,44 +15,84 @@
 #include "macro_utils.h"
 #include "maybe.h"
 
-/*
-Utilities to define an IterTake type for a specific element type and its corresponding iterator impl.
+/**
+ * @def IterTake(T)
+ * @brief Convenience macro to get the type of the IterTake struct with given element type.
+ *
+ * # Example
+ *
+ * @code
+ * DefineIterTake(int);
+ * IterTake(int) i; // Declares a variable of type IterTake(int)
+ * @endcode
+ *
+ * @param T The type of value the `Iterable` wrapped in this `IterTake` will yield. Must be the same type name passed to
+ * #DefineIterTake(T).
+ *
+ * @note If `T` is a pointer, it needs to be typedef-ed into a type that does not contain the `*`. Only alphanumerics.
+ */
+#define IterTake(T) IterTake##T
 
-An IterTake struct is a struct that keeps track of how many elements have been extracted out of an iterator
-as well as a limit to stop at - the iterator impl for this struct will keep consuming from an iterator until
-it hits that limit. (or if the source iterable has been exhausted)
-
-This allows to implement the `take_from` macro - which takes in the source iterable, and the number of elements
-to extract from it, and returns an iterable with an iterable of those elements.
-
-This is identical to the `take` function iterator typeclasses usually have.
-*/
-
-#define IterTake(ElmntType) IterTake##ElmntType
-
-#define DefineIterTake(ElmntType)                                                                                      \
+/**
+ * @def DefineIterTake(T)
+ * @brief Define an IterTake struct that works on `Iterable(int)`s.
+ *
+ * # Example
+ *
+ * @code
+ * DefineIterTake(int); // Defines an IterTake(int) struct
+ * @endcode
+ *
+ * @param T The type of value the `Iterable` wrapped in this `IterTake` will yield.
+ *
+ * @note If `T` is a pointer, it needs to be typedef-ed into a type that does not contain the `*`. Only alphanumerics.
+ * @note An #Iterator(T) for the given `T` **must** also exist.
+ */
+#define DefineIterTake(T)                                                                                              \
     typedef struct                                                                                                     \
     {                                                                                                                  \
         size_t i;                                                                                                      \
         size_t const limit;                                                                                            \
-        Iterable(ElmntType) const src;                                                                                 \
-    } IterTake(ElmntType)
+        Iterable(T) const src;                                                                                         \
+    } IterTake(T)
 
-/*
-Define the iterator implementation function for an IterTake struct
-
-The function is named `prep_itertake_of(ElmntType)`
-*/
-#define define_itertake_func(ElmntType, Name)                                                                          \
-    static Maybe(ElmntType) CONCAT(IterTake(ElmntType), _nxt)(IterTake(ElmntType) * self)                              \
+/**
+ * @def define_itertake_func(T, Name)
+ * @brief Define a function to turn given `IterTake` into an #Iterable(T).
+ *
+ * Define the `next` function implementation for the `IterTake` struct, and use it to implement the Iterator typeclass
+ * for a #IterTake(T), for given `T`.
+ *
+ * The defined function takes in a value of `IterTake(T)*` and wraps it in an `Iterable(T)`.
+ *
+ * # Example
+ *
+ * @code
+ * DefineIterTake(int);
+ *
+ * // Implement `Iterator` for `IterTake(int)`
+ * // The defined function has the signature- `Iterable(int) wrap_intitrtk(IterTake(int)* x)`
+ * define_itertake_func(int, wrap_intitrtk)
+ * @endcode
+ *
+ * @param T The type of value the `Iterable` wrapped in this `IterTake` will yield.
+ * @param Name Name to define the function as.
+ *
+ * @note If `T` is a pointer, it needs to be typedef-ed into a type that does not contain the `*`. Only
+ * alphanumerics.
+ * @note A #IterTake(T) for the given `T` **must** exist.
+ * @note This should not be delimited by a semicolon.
+ */
+#define define_itertake_func(T, Name)                                                                                  \
+    static Maybe(T) CONCAT(IterTake(T), _nxt)(IterTake(T) * self)                                                      \
     {                                                                                                                  \
         if (self->i < self->limit) {                                                                                   \
             ++(self->i);                                                                                               \
-            Iterable(ElmntType) srcit = self->src;                                                                     \
+            Iterable(T) srcit = self->src;                                                                             \
             return srcit.tc->next(srcit.self);                                                                         \
         }                                                                                                              \
-        return Nothing(ElmntType);                                                                                     \
+        return Nothing(T);                                                                                             \
     }                                                                                                                  \
-    impl_iterator(IterTake(ElmntType)*, ElmntType, Name, CONCAT(IterTake(ElmntType), _nxt))
+    impl_iterator(IterTake(T)*, T, Name, CONCAT(IterTake(T), _nxt))
 
 #endif /* !IT_TAKE_H */
