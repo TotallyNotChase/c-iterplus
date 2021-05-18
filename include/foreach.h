f6d30ede5317beb31eef8262bba4ac068d816efe
Chase <44284917+TotallyNotChase@users.noreply.github.com>
2021-05-18 22:09:55 +0700

    Add documentation for the `foreach` macro

--- include/foreach.h
+++ include/foreach.h
@@ -1,3 +1,8 @@
+/**
+ * @file
+ * Definition of the `foreach` macro.
+ */
+
 #ifndef LIB_ITPLUS_FOREACH_UTILS_H
 #define LIB_ITPLUS_FOREACH_UTILS_H
 
@@ -5,7 +10,18 @@
 
 #define UNIQVAR(x) CONCAT(CONCAT(x, _4x2_), __LINE__) /* "Unique" variable name */
 
-/* Iterate through given `it` iterable that contains elements of type `T` - store each element in `x` */
+/**
+ * @def foreach(T, x, it)
+ * @brief Iterate through given iterable and store each element in `x`
+ *
+ * @param T Type of the elements the iterable yields.
+ * @param x The variable name to store each element in. Available only inside the loop.
+ * @param it The iterable to iterate over. This will be consumed.
+ *
+ * @note `it` must not be an unevaluated expression. Otherwise, it will be evaluated multiple times in this macro.
+ * @note This macro cannot be used multiple times *in the same line*. Due to naming conflicts from an implicitly defined
+ * variable.
+ */
 #define foreach(T, x, it)                                                                                              \
     Maybe(T) UNIQVAR(res) = (it).tc->next((it).self);                                                                  \
     for (T x          = from_just_(UNIQVAR(res)); is_just(UNIQVAR(res));                                               \
