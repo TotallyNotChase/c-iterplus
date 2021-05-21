7617390c8a0f57e797127e41c066c60e460783e3
Chase <44284917+TotallyNotChase@users.noreply.github.com>
2021-05-21 22:26:17 +0530

    Add `take_while` and `drop_while` abstractions

---
+++ include/itplus_dropwhile.h
@@ -0,0 +1,104 @@
+/**
+ * @file
+ * @brief Macros for implementing the `takeWhile` abstraction using the `IterDropWhile` struct.
+ *
+ * https://hackage.haskell.org/package/base-4.15.0.0/docs/Data-List.html#v:dropWhile
+ * An IterDropWhile struct is a struct that stores a source iterable, a predicate function, and a flag to indicate
+ * when to stop testing with the predicate function. It keeps dropping from the source iterable until the
+ * predicate returns false, and starts returning the elements from the iterators from there on.
+ */
+
+#ifndef LIB_ITPLUS_DROPWHILE_H
+#define LIB_ITPLUS_DROPWHILE_H
+
+#include "itplus_foreach.h"
+#include "itplus_iterator.h"
+#include "itplus_macro_utils.h"
+#include "itplus_maybe.h"
+
+/**
+ * @def IterDropWhile(T)
+ * @brief Convenience macro to get the type of the IterDropWhile struct with given element type.
+ *
+ * # Example
+ *
+ * @code
+ * DefineIterDropWhile(int);
+ * IterDropWhile(int) i; // Declares a variable of type IterDropWhile(int)
+ * @endcode
+ *
+ * @param T The type of value the `Iterable` wrapped in this `IterDropWhile` will yield. Must be the same type name
+ * passed to #DefineIterDropWhile(T).
+ *
+ * @note If `T` is a pointer, it needs to be typedef-ed into a type that does not contain the `*`. Only alphanumerics.
+ */
+#define IterDropWhile(T) CONCAT(IterDropWhile, T)
+
+/**
+ * @def DefineIterDropWhile(T)
+ * @brief Define an IterDropWhile struct that works on `Iterable(T)`s.
+ *
+ * # Example
+ *
+ * @code
+ * DefineIterDropWhile(int); // Defines an IterDropWhile(int) struct
+ * @endcode
+ *
+ * @param T The type of value the `Iterable` wrapped in this `IterDropWhile` will yield.
+ *
+ * @note If `T` is a pointer, it needs to be typedef-ed into a type that does not contain the `*`. Only alphanumerics.
+ * @note An #Iterator(T) for the given `T` **must** also exist.
+ */
+#define DefineIterDropWhile(T)                                                                                         \
+    typedef struct                                                                                                     \
+    {                                                                                                                  \
+        bool (*pred)(T x);                                                                                             \
+        bool done;                                                                                                     \
+        Iterable(T) src;                                                                                               \
+    } IterDropWhile(T)
+
+/**
+ * @def define_iterdropwhile_func(T, Name)
+ * @brief Define a function to turn an #IterDropWhile(T) into an #Iterable(T).
+ *
+ * Define the `next` function implementation for the #IterDropWhile(T) struct, and use it to implement the Iterator
+ * typeclass, for given `T`.
+ *
+ * The defined function takes in a value of type `IterDropWhile(T)*` and wraps it in an `Iterable(T)`.
+ *
+ * # Example
+ *
+ * @code
+ * DefineIterDropWhile(int);
+ *
+ * // Implement `Iterator` for `IterDropWhile(int)`
+ * // The defined function has the signature- `Iterable(int) wrap_intitrdrpwhl(IterDropWhile(int)* x)`
+ * define_iterdropwhile_func(int, wrap_intitrdrpwhl)
+ * @endcode
+ *
+ * @param T The type of value the `Iterable` wrapped in this `IterDropWhile` will yield.
+ * @param Name Name to define the function as.
+ *
+ * @note If `T` is a pointer, it needs to be typedef-ed into a type that does not contain the `*`. Only
+ * alphanumerics.
+ * @note An #IterDropWhile(T) for the given `T` **must** exist.
+ * @note This should not be delimited by a semicolon.
+ */
+#define define_iterdropwhile_func(T, Name)                                                                             \
+    static Maybe(T) CONCAT(IterDropWhile(T), _nxt)(IterDropWhile(T) * self)                                            \
+    {                                                                                                                  \
+        if (self->done) {                                                                                              \
+            return Nothing(T);                                                                                         \
+        }                                                                                                              \
+        Iterable(T) srcit = self->src;                                                                                 \
+        foreach (T, x, srcit) {                                                                                        \
+            if (!self->pred(x)) {                                                                                      \
+                self->done = true;                                                                                     \
+                return Just(x, T);                                                                                     \
+            }                                                                                                          \
+        }                                                                                                              \
+        return Nothing(T);                                                                                             \
+    }                                                                                                                  \
+    impl_iterator(IterDropWhile(T)*, T, Name, CONCAT(IterDropWhile(T), _nxt))
+
+#endif /* !LIB_ITPLUS_DROPWHILE_H */
---
+++ include/itplus_takewhile.h
@@ -0,0 +1,102 @@
+/**
+ * @file
+ * @brief Macros for implementing the `takeWhile` abstraction using the `IterTakeWhile` struct.
+ *
+ * https://hackage.haskell.org/package/base-4.15.0.0/docs/Data-List.html#v:takeWhile
+ * An IterTakeWhile struct is a struct that stores a source iterable, a predicate function, and a flag to indicate
+ * when to stop testing with the predicate function. It keeps consuming from the source iterable, and stops once the
+ * predicate returns false.
+ */
+
+#ifndef LIB_ITPLUS_TAKEWHILE_H
+#define LIB_ITPLUS_TAKEWHILE_H
+
+#include "itplus_iterator.h"
+#include "itplus_macro_utils.h"
+#include "itplus_maybe.h"
+
+/**
+ * @def IterTakeWhile(T)
+ * @brief Convenience macro to get the type of the IterTakeWhile struct with given element type.
+ *
+ * # Example
+ *
+ * @code
+ * DefineIterTakeWhile(int);
+ * IterTakeWhile(int) i; // Declares a variable of type IterTakeWhile(int)
+ * @endcode
+ *
+ * @param T The type of value the `Iterable` wrapped in this `IterTakeWhile` will yield. Must be the same type name
+ * passed to #DefineIterTakeWhile(T).
+ *
+ * @note If `T` is a pointer, it needs to be typedef-ed into a type that does not contain the `*`. Only alphanumerics.
+ */
+#define IterTakeWhile(T) CONCAT(IterTakeWhile, T)
+
+/**
+ * @def DefineIterTakeWhile(T)
+ * @brief Define an IterTakeWhile struct that works on `Iterable(T)`s.
+ *
+ * # Example
+ *
+ * @code
+ * DefineIterTakeWhile(int); // Defines an IterTakeWhile(int) struct
+ * @endcode
+ *
+ * @param T The type of value the `Iterable` wrapped in this `IterTakeWhile` will yield.
+ *
+ * @note If `T` is a pointer, it needs to be typedef-ed into a type that does not contain the `*`. Only alphanumerics.
+ * @note An #Iterator(T) for the given `T` **must** also exist.
+ */
+#define DefineIterTakeWhile(T)                                                                                         \
+    typedef struct                                                                                                     \
+    {                                                                                                                  \
+        bool (*pred)(T x);                                                                                             \
+        bool done;                                                                                                     \
+        Iterable(T) src;                                                                                               \
+    } IterTakeWhile(T)
+
+/**
+ * @def define_itertakewhile_func(T, Name)
+ * @brief Define a function to turn an #IterTakeWhile(T) into an #Iterable(T).
+ *
+ * Define the `next` function implementation for the #IterTakeWhile(T) struct, and use it to implement the Iterator
+ * typeclass, for given `T`.
+ *
+ * The defined function takes in a value of type `IterTakeWhile(T)*` and wraps it in an `Iterable(T)`.
+ *
+ * # Example
+ *
+ * @code
+ * DefineIterTakeWhile(int);
+ *
+ * // Implement `Iterator` for `IterTakeWhile(int)`
+ * // The defined function has the signature- `Iterable(int) wrap_intitrtk(IterTakeWhile(int)* x)`
+ * define_itertakewhile_func(int, wrap_intitrtk)
+ * @endcode
+ *
+ * @param T The type of value the `Iterable` wrapped in this `IterTakeWhile` will yield.
+ * @param Name Name to define the function as.
+ *
+ * @note If `T` is a pointer, it needs to be typedef-ed into a type that does not contain the `*`. Only
+ * alphanumerics.
+ * @note An #IterTakeWhile(T) for the given `T` **must** exist.
+ * @note This should not be delimited by a semicolon.
+ */
+#define define_itertakewhile_func(T, Name)                                                                             \
+    static Maybe(T) CONCAT(IterTakeWhile(T), _nxt)(IterTakeWhile(T) * self)                                            \
+    {                                                                                                                  \
+        if (self->done) {                                                                                              \
+            return Nothing(T);                                                                                         \
+        }                                                                                                              \
+        Iterable(T) srcit = self->src;                                                                                 \
+        Maybe(T) res      = srcit.tc->next(srcit.self);                                                                \
+        if (is_nothing(res) || !self->pred(from_just_(res))) {                                                         \
+            self->done = true;                                                                                         \
+            return Nothing(T);                                                                                         \
+        }                                                                                                              \
+        return res;                                                                                                    \
+    }                                                                                                                  \
+    impl_iterator(IterTakeWhile(T)*, T, Name, CONCAT(IterTakeWhile(T), _nxt))
+
+#endif /* !LIB_ITPLUS_TAKEWHILE_H */
