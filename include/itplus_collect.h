3455463e3aa89d52f31ba805571ab0d9e9b16bbb
Chase <44284917+TotallyNotChase@users.noreply.github.com>
2021-05-22 18:43:08 +0530

    Add `collect` abstraction

---
+++ include/itplus_collect.h
@@ -0,0 +1,60 @@
+/**
+ * @file
+ * @brief Macros for implementing the `collect` abstraction.
+ *
+ * https://doc.rust-lang.org/std/iter/trait.Iterator.html#method.collect
+ * Note: Rust's `collect` has a polymorphic return type. The `collect` here just turns an iterable into an array.
+ */
+
+#ifndef LIB_ITPLUS_COLLECT_H
+#define LIB_ITPLUS_COLLECT_H
+
+#include "itplus_foreach.h"
+#include "itplus_iterator.h"
+#include "itplus_maybe.h"
+
+#include <stdlib.h>
+
+#define ITPLUS_COLLECT_BUFSZ 64
+
+/**
+ * @def define_itercollect_func(T, Name)
+ * @brief Define the `collect` function for an iterable.
+ *
+ * The defined function takes in an iterable of type `T`, and turns it into an array. Each element of said array
+ * is of type `T`.
+ *
+ * This defined function will consume the given iterable.
+ *
+ * The returned array must be freed.
+ *
+ * @param T The type of value the `Iterable`, for which this is being implemented, yields.
+ * @param Name Name to define the function as.
+ *
+ * @note If `T` is a pointer, it needs to be typedef-ed into a type that does not contain the `*`. Only alphanumerics.
+ * @note An #Iterator(T) for the given `T` **must** also exist.
+ */
+#define define_itercollect_func(T, Name)                                                                               \
+    T* Name(Iterable(T) it, size_t* len)                                                                               \
+    {                                                                                                                  \
+        size_t size = ITPLUS_COLLECT_BUFSZ;                                                                            \
+        *len        = 0;                                                                                               \
+        T* arr      = malloc(size * sizeof(*arr));                                                                     \
+        if (arr == NULL) {                                                                                             \
+            return NULL;                                                                                               \
+        }                                                                                                              \
+        foreach (T, x, it) {                                                                                           \
+            if (*len == size) {                                                                                        \
+                T* temp = realloc(arr, (size *= 2) * sizeof(*arr));                                                    \
+                if (temp == NULL) {                                                                                    \
+                    free(arr);                                                                                         \
+                    return NULL;                                                                                       \
+                }                                                                                                      \
+                arr = temp;                                                                                            \
+            }                                                                                                          \
+            arr[(*len)++] = x;                                                                                         \
+        }                                                                                                              \
+        return arr;                                                                                                    \
+    }
+
+#endif /* !LIB_ITPLUS_COLLECT_H */
