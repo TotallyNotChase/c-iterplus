0afdb2ec4db4a6e50c2b59f681257a743269aae1
Chase <44284917+TotallyNotChase@users.noreply.github.com>
2021-05-23 18:55:03 +0530

    Add `enumerate` abstraction

---
+++ include/itplus_enumerate.h
@@ -0,0 +1,100 @@
+/**
+ * @file
+ * @brief Macros for implementing the `zip` abstraction using the `IterEnumr` struct.
+ *
+ * https://hackage.haskell.org/package/base-4.15.0.0/docs/Data-List.html#v:zip
+ * An IterEnumr struct is a struct that stores an iterable, and its iteration index. Its `next` function implementation
+ * that returns a `Pair` of 2 elements, first being the index, and second being an element from the source iterable.
+ */
+
+#ifndef LIB_ITPLUS_ENUMR_H
+#define LIB_ITPLUS_ENUMR_H
+
+#include "itplus_iterator.h"
+#include "itplus_macro_utils.h"
+#include "itplus_maybe.h"
+#include "itplus_pair.h"
+
+#include <stddef.h>
+
+/**
+ * @def IterEnumr(T, U)
+ * @brief Convenience macro to get the type of the IterEnumr struct with given element types.
+ *
+ * # Example
+ *
+ * @code
+ * DefineIterEnumr(int);
+ * IterEnumr(int) i; // Declares a variable of type IterEnumr(int)
+ * @endcode
+ *
+ * @param T The type of value the `Iterable` wrapped in this `IterEnumr` will yield. Must be the same type name
+ * passed to #DefineIterEnumr(T).
+ *
+ * @note If `T` is a pointer, it needs to be typedef-ed into a type that does not contain the `*`. Only
+ * alphanumerics.
+ */
+#define IterEnumr(T) CONCAT(IterEnumr, T)
+
+/**
+ * @def DefineIterEnumr(T)
+ * @brief Define an IterEnumr struct that works with on `Iterable(T)`s.
+ *
+ * # Example
+ *
+ * @code
+ * DefineIterEnumr(int); // Defines an IterEnumr(int) struct
+ * @endcode
+ *
+ * @param T The type of value the `Iterable` wrapped in this `IterEnumr` will yield.
+ *
+ * @note If `T` is a pointer, it needs to be typedef-ed into a type that does not contain the `*`. Only
+ * alphanumerics.
+ * @note An #Iterator(T) for the given `T` **must** also exist.
+ */
+#define DefineIterEnumr(T)                                                                                             \
+    typedef struct                                                                                                     \
+    {                                                                                                                  \
+        size_t i;                                                                                                      \
+        Iterable(T) src;                                                                                               \
+    } IterEnumr(T)
+
+/**
+ * @def define_iterenumr_func(T, Name)
+ * @brief Define a function to turn an #IterEnumr(T) into an #Iterable(T) where `T = Pair(size_t, T)`.
+ *
+ * Define the `next` function implementation for the #IterEnumr(T) struct, and use it to implement the Iterator
+ * typeclass, for given `T`.
+ *
+ * The defined function takes in a value of type `IterEnumr(T)*` and wraps it in an `Iterable(Pair(size_t, T))`.
+ *
+ * # Example
+ *
+ * @code
+ * DefineIterEnumr(int);
+ *
+ * // Implement `Iterator` for `IterEnumr(int)`
+ * // The defined function has the signature- `Iterable(Pair(size_t, int)) wrap_intenumr(IterEnumr(int)* x)`
+ * define_iterenumr_func(int, wrap_intenumr)
+ * @endcode
+ *
+ * @param T The type of value the first `Iterable` wrapped in this `IterEnumr` will yield.
+ * @param Name Name to define the function as.
+ *
+ * @note If `T` is a pointer, it needs to be typedef-ed into a type that does not contain the `*`. Only
+ * alphanumerics.
+ * @note An #IterEnumr(T) for the given `T` **must** exist.
+ * @note An #Iterator(T), with `T = Pair(size_t, T)`, for the given `T`.
+ * @note This should not be delimited by a semicolon.
+ */
+#define define_iterenumr_func(T, Name)                                                                                 \
+    static Maybe(Pair(size_t, T)) CONCAT(IterEnumr(T), _nxt)(IterEnumr(T) * self)                                      \
+    {                                                                                                                  \
+        Iterable(T) srcit = self->src;                                                                                 \
+        Maybe(T) res      = srcit.tc->next(srcit.self);                                                                \
+        return is_just(res) ? Just(PairOf(self->i++, from_just_(res), size_t, T), Pair(size_t, T))                     \
+                            : Nothing(Pair(size_t, T));                                                                \
+    }                                                                                                                  \
+    impl_iterator(IterEnumr(T)*, Pair(size_t, T), Name, CONCAT(IterEnumr(T), _nxt))
+
+#endif /* !LIB_ITPLUS_ENUMR_H */
---
+++ include/itplus_pair.h
@@ -0,0 +1,89 @@
+/**
+ * @file
+ * Utilities to define and use a Pair type. A Tuple of 2 elements.
+ *
+ * https://hackage.haskell.org/package/base-4.15.0.0/docs/Data-Tuple.html
+ */
+
+#ifndef LIB_ITPLUS_PAIR_H
+#define LIB_ITPLUS_PAIR_H
+
+#include "itplus_macro_utils.h"
+
+/**
+ * @def Pair(T, U)
+ * @brief Convenience macro to get the type of the Pair defined with certain types.
+ *
+ * # Example
+ *
+ * @code
+ * DefinePair(int, char);
+ * Pair(int, char) const x = {0}; // Uses the pair type defined in the previous line
+ * @endcode
+ *
+ * @param T The type the first value in this `Pair` struct. Must be the same type name passed to #DefinePair(T, U)
+ * @param T The type the second value in this `Pair` struct. Must be the same type name passed to #DefinePair(T, U).
+ *
+ * @note If `T`, or `U`, is a pointer, it needs to be typedef-ed into a type that does not contain the `*`. Only
+ * alphanumerics.
+ */
+#define Pair(T, U) CONCAT(CONCAT(Pair, T), U)
+
+/**
+ * @def DefinePair(T, U)
+ * @brief Define a Pair<T, U> type.
+ *
+ * # Example
+ *
+ * @code
+ * DefinePair(int, char); // Defines a Pair(int, char) type
+ * @endcode
+ *
+ * @param T The type the first value in this `Pair` struct.
+ * @param T The type the second value in this `Pair` struct.
+ *
+ * @note If `T`, or `U`, is a pointer, it needs to be typedef-ed into a type that does not contain the `*`. Only
+ * alphanumerics.
+ */
+#define DefinePair(T, U)                                                                                               \
+    typedef struct                                                                                                     \
+    {                                                                                                                  \
+        T a;                                                                                                           \
+        U b;                                                                                                           \
+    } Pair(T, U)
+
+/**
+ * @def PairOf(x, y, T, U)
+ * @brief Wrap 2 values into a #Pair(T, U).
+ *
+ * # Example
+ *
+ * @code
+ * DefinePair(int, char);
+ * Pair(int, char) const x = PairOf(42, 'c', int, char); // Initializes a Pair(int, char) with the value 42 and 'c'
+ * @endcode
+ *
+ * @param x The value to put as the first member of the pair struct.
+ * @param y The value to put as the second member of the pair struct.
+ * @param T The type of the first value.
+ * @param U The type of the second value.
+ *
+ * @note If `T`, or `U`, is a pointer, it needs to be typedef-ed into a type that does not contain the `*`. Only alphanumerics.
+ * @note A #Pair(T, U) for given `T` and `U` must exist.
+ * @note The values are simply assigned to the #Pair(T, U) struct. No implicit copying is done.
+ */
+#define PairOf(x, y, T, U) ((Pair(T, U)){.a = (x), .b = (y)})
+
+/**
+ * @def fst(pair)
+ * @brief Get the first value from given pair.
+ */
+#define fst(pair) (pair).a
+
+/**
+ * @def snd(pair)
+ * @brief Get the second value from given pair.
+ */
+#define snd(pair) (pair).b
+
+#endif /* !LIB_ITPLUS_PAIR_H */
